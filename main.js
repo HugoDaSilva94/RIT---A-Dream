a8a6f038420f4524dae011c698fff1afdcc6041c
Rachel <rwong3@stuy.edu>
2022-05-13 05:18:44 -0400

    random selection of places from omeka database completed

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 let canvas;
 let ctx;
+let pages = document.getElementsByClassName("page");
 
 
 window.onload = () => {
@@ -9,18 +10,23 @@ window.onload = () => {
 }
 
 function init() {
-    // Rudimentary canvas initialization
-    canvas = document.getElementById('content');
-    if (canvas.getContext) ctx = canvas.getContext('2d');
-    ctx.canvas.width = window.innerWidth;
-    ctx.canvas.height = window.innerHeight;
-    ctx.fillStyle = '#000000';
-    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
     // Get 7 locations
+
+    // Hide all content except title
 
     // Get contents (related items) of locations
 
     // Draw all location content, but set them all to hidden, except for the title
+}
+
+function getSevenRandomPlaces(jsonFile) {
+    d3.json(jsonFile).then((data) => {
+        // Get place data from Omeka
+		let placeLayers = data.layers.filter(layers=>layers.class["o:label"]=="Lieu");
+        // Shuffle data
+        let shuffled = placeLayers.sort(()=> 0.5 - Math.random());
+        let selected = shuffled.slice(0,7);
+        console.log(selected);
+	});
 }
 
